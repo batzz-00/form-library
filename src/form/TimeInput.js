a1bfc469d64ad11cd3d857d404a1e5e72cba237b
batzz00 <ellerybd@cardiff.ac.uk>
2019-02-25 21:52:08 +0000

    Moved all the code into a helper file

--- src/form/TimeInput.js
+++ src/form/TimeInput.js
@@ -8,6 +8,9 @@ import { withHandler } from './withHandler'
 // Style
 import './form.sass'
 
+// Helpers
+import InputSpacer from '../helpers/inputSpacer'
+
 // React Components
 import Errors from './Errors'
 
@@ -18,77 +21,17 @@ class TimeInput extends React.Component {
     this.actualValue = ''
     this.handleChange = this.handleChange.bind(this)
     this.onKeyDown = this.onKeyDown.bind(this)
+    this.inputSpacer = new InputSpacer(' ', 2,4)
   }
   handleChange (e) {
-    let blockDelimiter = "\|"
-
-    if (blockDelimiter.charCodeAt(0) === this.lastKey.charCodeAt(0)) {
-      return false
-    }
-
-    const { displayValue } = this.state
-    const { actualValue } = this
-    // options
-
-    let it = 0
-    let blocks = 2
-    let delimiterSize = 5
-    let maxLength = 11
-
-    let reg = new RegExp(blockDelimiter.replace(blockDelimiter, '\\$&'), 'g')
-    console.log(e.target.value.replace(reg, ''))
-    console.log(reg)
-    let val = e.target.value.replace(reg, '').split('').slice(0, maxLength).join('')
-
-    let diff = Math.abs(this.selectionStart - this.selectionEnd)
-
-    if (this.lastKey === 'Backspace') {
-      val = diff !== 0 ? displayValue.split('').slice(0, displayValue.length - diff).join('').replace(reg, '')
-        : actualValue.split('').splice(0, actualValue.length - 1).join('')
-    }
-
-    let blockedOutput = []
-    let split = val.split('')
-    let count = 0
-    for (let i in val.split('')) {
-      if (val.split('').length % 2 === 0 && parseInt(i) === val.split('').length - 1) {
-        break
-      }
-      if (it % blocks === 1) {
-        blockedOutput.push(new Array(delimiterSize).fill(blockDelimiter))
-      } else {
-        blockedOutput[it] = []
-        for (let b = 0; b <= blocks - 1; b++) {
-          blockedOutput[it].push(split.splice(0, 1)[0])
-        }
-        it++
-      }
-      count++
-      if (blocks === count) {
-        it++
-        count = 0
-      }
-    }
-    console.log(blockedOutput)
-
-    val = blockedOutput.length === 0 ? '' : blockedOutput.reduce((p, n) => { return p.concat(n) }).join('')
-    this.actualValue = val.replace(reg, '')
-    this.setState({ displayValue: val })
-    // if(newActualValue.length >= maxLength){
-    //   if(this.lastKey === "Backspace") {
-    //     this.actualValue = newActualValue
-    //     this.setState({ displayValue: val})
-    //   }
-    // } else {
-    //   this.actualValue = newActualValue
-    //   this.setState({ displayValue: val})
-    // }
+    this.inputSpacer.spaceInput(e.target.value)
+    console.log(this.inputSpacer.displayValue)
+    this.setState({displayValue: this.inputSpacer.displayValue})
   }
   onKeyDown (e) {
-    this.lastKey = e.key
-    this.selectionStart = e.target.selectionStart
-    this.selectionEnd = e.target.selectionEnd
-    this.selectionDirection = e.target.selectionDirection
+    this.inputSpacer.setLastKey(e.key)
+    this.inputSpacer.setSelectionStart(e.target.selectionStart)
+    this.inputSpacer.setSelectionEnd(e.target.selectionEnd)
   }
   render () {
     const { title, name, placeholder, type, errors } = this.props
